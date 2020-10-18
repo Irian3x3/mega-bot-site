f3c6d36b800f6e653a7f3df4e78211f2f604ca21
Irian3x3 <68793401+Irian3x3@users.noreply.github.com>
2020-10-18 14:04:14 -0400

    .

--- index.php
+++ index.php
@@ -8,13 +8,30 @@
     <body>
         <header>
             <div class="topnav">
-                    <a href="#">Home</a> 
-                    <a href="http://gg.gg/invite-mega-bot">Invite</a> 
-                    <a href="https://invite.gg/iriandev">Support Server</a>
-                    <a style="float:right;" href="https://github.com/Irian3x3/Mega">GitHub Repo</a>
-                    <a style="float:right;"href="./vote">Vote</a> 
+            <h1 id="megaWord">Mega</h1>
+                    <a href="#" title="Home">Home</a> 
+                    <a href="http://gg.gg/invite-mega-bot" title="Invite Mega!">Invite</a> 
+                    <a href="https://invite.gg/iriandev" title="Need help? Join the support server!">Support Server</a>
+                    <a style="float:right;" href="https://github.com/Irian3x3/Mega" title="Mega's Open Source Repository">GitHub Repo</a>
+                    <a style="float:right;"href="./vote" title="Vote for Mega">Vote</a>
             </div>
+            <div class="widgets">
+                    <a href="https://top.gg/bot/746855438296809522" title="Library">
+            <img src="https://top.gg/api/widget/lib/746855438296809522.svg?noavatar=true" alt="Mega" />
+        </a>
+            <a href="https://top.gg/bot/746855438296809522" title="Upvotes">
+            <img src="https://top.gg/api/widget/upvotes/746855438296809522.svg?noavatar=true" alt="Mega"/>
+            <a href="https://top.gg/bot/746855438296809522" title="Status">
+            <img src="https://top.gg/api/widget/status/746855438296809522.svg?noavatar=true" alt="Mega" />
+        </a>
+            <a href="https://top.gg/bot/746855438296809522" title="Bot Owner">
+            <img src="https://top.gg/api/widget/owner/746855438296809522.svg?noavatar=true" alt="Mega"/>
+        </a>
+        <a href="https://top.gg/bot/746855438296809522" title="Server Count">
+            <img src="https://top.gg/api/widget/servers/746855438296809522.svg?noavatar=true" alt="Mega"/>
+        </a>
+        </a>
+</div>
         </header>
-        <h1 id="megaWord">Mega</h1>
     </body>
 </html>
