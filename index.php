884c1b1aa963811f48180b9d7439419b6764a896
Irian3x3 <68793401+Irian3x3@users.noreply.github.com>
2020-10-18 21:00:06 -0400

    center the description and remove the underline from the widgets

--- index.php
+++ index.php
@@ -1,7 +1,7 @@
 <!DOCTYPE html>
 <html>
     <head>
-    <title>Home | Mega</title>
+      <title>Home | Mega</title>
 
         <link rel="stylesheet" href="./css/ids.css">
         <link rel="stylesheet" href="./css/main.css">
@@ -14,29 +14,69 @@
         <header>
             <div class="topnav">
             <h1 id="megaWord">Mega</h1>
-                    <a href="/" title="Home">Home</a> 
-                    <a href="http://gg.gg/invite-mega-bot" title="Invite Mega!">Invite</a> 
-                    <a href="https://invite.gg/iriandev" title="Need help? Join the support server!">Support Server</a>
-                    <a style="float:right;" href="https://github.com/Irian3x3/Mega" title="Mega's Open Source Repository">GitHub Repo</a>
-                    <a style="float:right;"href="./vote" title="Vote for Mega">Vote</a>
+            <a href="/" title="Home">Home</a> 
+            <a href="http://gg.gg/invite-mega-bot" title="Invite Mega!">Invite</a> 
+            <a href="https://invite.gg/iriandev" title="Need help? Join the support server!">Support Server</a>
+            <a style="float:right;" href="https://github.com/Irian3x3/Mega" title="Mega's Open Source Repository">GitHub Repo</a>
+            <a style="float:right;"href="./vote" title="Vote for Mega">Vote</a>
             </div>
-            <div class="widgets">
-                    <a href="https://top.gg/bot/746855438296809522" title="Library">
-            <img src="https://top.gg/api/widget/lib/746855438296809522.svg?noavatar=true" alt="Mega" />
-        </a>
-            <a href="https://top.gg/bot/746855438296809522" title="Upvotes">
-            <img src="https://top.gg/api/widget/upvotes/746855438296809522.svg?noavatar=true" alt="Mega"/>
-            <a href="https://top.gg/bot/746855438296809522" title="Status">
-            <img src="https://top.gg/api/widget/status/746855438296809522.svg?noavatar=true" alt="Mega" />
-        </a>
-            <a href="https://top.gg/bot/746855438296809522" title="Bot Owner">
-            <img src="https://top.gg/api/widget/owner/746855438296809522.svg?noavatar=true" alt="Mega"/>
-        </a>
-        <a href="https://top.gg/bot/746855438296809522" title="Server Count">
-            <img src="https://top.gg/api/widget/servers/746855438296809522.svg?noavatar=true" alt="Mega"/>
-        </a>
-        </a>
+            <div class="desc">
+            <h1 style="text-align:center;">Mega</h1>
+    <!--Widgets thingies-->
+    <div class="widgets">
+      <a href="https://top.gg/bot/746855438296809522">
+        <img src="https://top.gg/api/widget/lib/746855438296809522.svg?noavatar=true" alt="Mega" />
+      </a>
+      <a href="https://top.gg/bot/746855438296809522">
+        <img src="https://top.gg/api/widget/upvotes/746855438296809522.svg?noavatar=true" alt="Mega"/>
+        <img src="https://top.gg/api/widget/status/746855438296809522.svg?noavatar=true" alt="Mega" />
+      </a>
+      <a href="https://top.gg/bot/746855438296809522">
+        <img src="https://top.gg/api/widget/owner/746855438296809522.svg?noavatar=true" alt="Mega"/>
+      </a>
+    </div>
+  <!--Widgets thingies end here :D-->
+  
+  <h3 style="text-align:center">Who is Mega?</h3>
+  <p>
+  <em><b>Mega</b> is a multi-purpose bot, mainly being based around moderation. It does have more than just that, such as:
+    <ul>
+      <li>
+        Simple moderation commands such as <code>kick</code> or <code>ban</code> commands
+      </li>
+      <li>
+        Fun commands such as <code>lenny</code> generators, or <code>reddit</code> search command
+      </li>
+      <li>
+        Info commands such as <code>serverinfo</code> or <code>userinfo</code> commands (userinfo do be broken tho)
+      </li>
+    </ul></em>
+  </p>
+  <h3 style="text-align:center;">Why should I invite Mega?</h3>
+  <ul>
+  	<li>
+      ✅ 24/7 Online
+  	</li>
+  	<li>
+    ✅ Very good and working commands
+  	</li>
+  	<li>
+    ✅ Fast response
+  	</li>
+  	<li>
+     ✅ Open Source
+  	</li>
+  	<li>
+     ✅ Actively Updated
+  	</li>
+  	<li>
+      And more, but that's for <em>you</em> to figure out!
+  	</li>
+  </ul>
 </div>
+  </body>
+  
+ </html>
         </header>
     </body>
 </html>
