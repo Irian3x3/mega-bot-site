2691a4635df97d6ab2cdb8c07a9368b1a35495fc
Irian3x3 <68793401+Irian3x3@users.noreply.github.com>
2020-11-13 09:49:57 -0400

    thanks heroku

---
+++ index.php
@@ -0,0 +1,9 @@
+<!doctype html>
+<html>
+    <head>
+        <meta http-equiv="refresh" content="1;/index.html">
+    </head>
+     <p>
+         You are being redirected to index.html because heroku is literally forcing me to have a php file
+    </p>
+</html>
